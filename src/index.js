ea3daa55813a6cd04050c5fee540a1e3e146a452
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-10-06 14:00:39 +0100

    ok

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ import { server } from 'socket.io'
 const app=express()
 app.use(cors())
 app.use(express.json())
+
+const http = http.createServer(app)
+
+httpServer.listen(3030, ()=>{
+  console.log("Server is listening on Port 3030")
+}) 
